b2a8e264187d9f4bbfd95f77b19b51f45db5f9e7
Kwonminwoo <qwerty029369@naver.com>
2022-05-27 23:44:13 +0900

    test-commit

--- src/main/java/hello/core2/member/MemberServiceImpl.java
+++ src/main/java/hello/core2/member/MemberServiceImpl.java
@@ -7,7 +7,7 @@ package hello.core2.member;
 public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();// 추상화에도 의존, 구현체에도 의존 DIP를 위반하는 코드
 
-
+    // tt
     @Override
     public void join(Member member) {
         memberRepository.save(member);
