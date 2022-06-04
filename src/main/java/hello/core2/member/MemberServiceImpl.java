4fd0d58ed14e98d99a928d69140120190c4f9c9b
Kwonminwoo <qwerty029369@naver.com>
2022-06-04 23:41:45 +0900

    8th-commit 의존관계 주입, 롬복(lombok) 사용 6/4

--- src/main/java/hello/core2/member/MemberServiceImpl.java
+++ src/main/java/hello/core2/member/MemberServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
  *  서비스 구현체 클래스 implement되는 대상이 하나 일 떄는 Impl을 써서 표시
  *  MemberService -> MemberServiceImpl 로 MemberServiceImpl인터페이스에서 상속되는 클래스는 MemberServiceImpl하나 뿐
  */
+
 @Component // 컴포넌트스캔을 위해 컴포넌트로 설정
 public class MemberServiceImpl implements MemberService{
     //private final MemberRepository memberRepository = new MemoryMemberRepository();// 추상화에도 의존, 구현체에도 의존 DIP를 위반하는 코드
--- src/main/java/hello/core2/order/OrderServiceImpl.java
+++ src/main/java/hello/core2/order/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import hello.core2.discount.FixDiscountPolicy;
 import hello.core2.member.Member;
 import hello.core2.member.MemberRepository;
 import hello.core2.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,17 +13,19 @@ import org.springframework.stereotype.Component;
  *  주문서비스 구현체 클래스
  */
 @Component
+@RequiredArgsConstructor // final이 붙은 필수 필드를 이용한 생성자를 만들어 줌
 public class OrderServiceImpl implements OrderService{
     //private final MemberRepository memberRepository = new MemoryMemberRepository(); // 기존의 직접 객체를 생성하던 코드
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+    // 롬복으로 생성자가 자동으로 만들어져 기존의 코드는 필요 x
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
